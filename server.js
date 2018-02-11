0eb84cc1d3f07b292964153946b9f4bd202dbbcd
Lee, Adam <Adam.Lee1@edelman.com>
2018-02-11 18:00:42 +0000

    Update path for static files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Serve static content via Node (source: https://medium.com/shovel-apps/simple-node-js-server-on-heroku-210ec24f485)
 const static = require('node-static');
-var file = new static.Server();
+var file = new static.Server('./public');
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response);

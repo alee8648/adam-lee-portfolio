3d705ef1feca96b16de0a2ebbc79969a77b809c5
Lee, Adam <Adam.Lee1@edelman.com>
2018-02-11 17:49:47 +0000

    Set up Heroku as a node server that servers static content

---
+++ server.js
@@ -0,0 +1,8 @@
+// Serve static content via Node (source: https://medium.com/shovel-apps/simple-node-js-server-on-heroku-210ec24f485)
+const static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);

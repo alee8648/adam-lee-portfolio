ffc6470dc746fc1b1b10f920af01942cc1e2e8c3
Lee, Adam <Adam.Lee1@edelman.com>
2018-02-11 17:33:35 +0000

    Convert all my CV content into a JS file that outputs JSON

--- dev/content/content.js
+++ dev/content/content.js
@@ -1,3 +1,167 @@
 module.exports = {
-	"title" : "Adam Lee | Full Stack Web Developer"
+	"title" : "Adam Lee | Full Stack Web Developer",
+	"intro"	: "Fast-learning full stack web developer with a diverse background, a First Class Honours degree from Durham University, and a curiosity for all things digital. Having broadened my skill-set in a creative tech agency with major international clients, I am looking for an exciting opportunity to fulfil an impactful role in a sector-leading team.",
+	"contact" : {
+		"phoneLocal" : "07913041037",
+		"phoneInternational" : "+447913041037",
+		"email"	: "alee8648@gmail.com"
+	},
+	"roles" : [
+		{
+			"position"				: "Technical Developer",
+			"company"				: "Edelman Deportivo",
+			"companyDescription"	: "Creative technology agency within international PR firm",
+			"date"					: "June 2016 -",
+			"points"				: [
+				"Node.js API for a chatbot for LEGO, praised by Facebook as the best commercial use of a chatbot they had seen",
+				"CMS sites in WordPress and Drupal for Edelman, GE, and Geneva Association",
+				"Third-party API integration for a large-scale research campaign for Shell",
+				"Regularly lead small projects that I worked on independently or with junior colleagues, such as a review code distribution interface site for Xbox",
+				"Communicated with clients to offer updates, field questions, and make suggestions",
+				"Assisted in scoping new projects, drawing up timelines, and creating proposals",
+				"Trained external and internal clients in using their new WordPress sites",
+				"Extended and improved poorly-written, legacy code",
+				"Applied my copywriting skills to support internal projects and proposals",
+				"Promised a promotion to senior technical developer in the new year"
+			]
+		},
+		{
+			"position"				: "Junior Front End Web Developer",
+			"company"				: "Judd Associates",
+			"companyDescription"	: "Web & Digital Marketing Agency, Crowborough",
+			"date"					: "Nov 2015 - June 2016",
+			"points"				: [
+				"Developed custom WordPress themes from PSD designs using an in-house grid framework, ensuring cross-device and -browser compatibility back to IE9",
+				"Over short space of time developed the ability to confidently and accurately build sites in a streamlined, self-imposed process",
+				"Took over SEO responsibilities to help with staff shortfall, including writing content, monitoring keyword rankings, link building, and on-site optimisation"
+			]
+		},
+		{
+			"position"				: "Web Developer",
+			"company"				: "UOW Limited",
+			"companyDescription"	: "Online casual clothing retailer",
+			"date"					: "May - Aug 2015",
+			"points"				: [
+				"Planned the development schedule and organised my own research to a deadline as sole developer in charge of a new website project based on a WooCommerce theme",
+				"As my first web build, I developed the site simultaneously with my own learning curve",
+				"Used plug-ins to add new back-end features for the client, including an import plug-in to manage stock, maintain product listings across multiple platforms, and import existing data into the site",
+				"Created interactive, easy-to-use Excel worksheets with custom-coded functions to resolve a stock and listings management conflict with the existing Amazon-based store"
+			]
+		},
+		{
+			"position"				: "Research Analyst Intern",
+			"company"				: "Media Measurement",
+			"companyDescription"	: "Social media analytics",
+			"date"					: "May - Jul 2014",
+			"points"				: [
+				"Quickly learnt to use Salesforce's Radian6 social listening system, assisting on two team projects to produce reports for client Reckitt Benckiser within my first week",
+				"Acted independently to compile a list of recommendations for the improvement of the Radian6 software, optimising workflow processes within the company to save time and avoid inaccuracies",
+				"Identified social media personas and how they interacted with the brand to advise the client on how to effectively engage different demographics",
+				"Analysed and critiqued effectiveness of social media campaigns for FMCG brands, gaining insight on customer/market usage of social platforms"
+			]
+		},
+		{
+			"position"				: "Marketing and Social Media Assistant",
+			"company"				: "BANG+STRIKE",
+			"companyDescription"	: "Online retailer of men's underwear",
+			"date"					: "Aug 2014 - Feb 2015",
+			"points"				: [
+				"Principal blog-writer, producing creative, SEO-friendly content on WordPress",
+				"Responsible for producing and scheduling content for the site's social channels"
+			]
+		},
+		{
+			"position"				: "Copywriter, Blogger, and Social Media Consultant",
+			"company"				: "Freelance work",
+			"date"					: "Sep 2014 - May 2015",
+			"points"				: [
+				"Took on a large project for Patchworks.co.uk (digital agency) writing enticing and descriptive landing pages for prospective clients and to optimise for SEO",
+				"Optimised Amazon listings for University of Whatever and Oneskee",
+				"Edited and rewrote customer communication for Hampton Dean financial advisors"
+			]
+		},
+		{
+			"position"				: "Amazon Marketing Specialist",
+			"company"				: "UOW Limited",
+			"companyDescription"	: "Online casual clothing retailer",
+			"date"					: "Aug 2013 - May 2014",
+			"points"				: [
+				"Personally doubled the brand's Amazon traffic",
+				"Analysed sales, page views, and conversion rates through weekly reports and the creation of a database",
+				"Managed Amazon listings - adding new products and optimising existing stock",
+				"Responsible for all copywriting - the website, product descriptions, brand message"
+			]
+		}
+	],
+	"education" : [
+		{
+			"location"				: "University of Durham",
+			"description"			: "First Class BA(Hons) Modern Languages and Cultures (Spanish & Italian)",
+			"date"					: "Oct 2009 - Jun 2013",
+			"points"				: [
+				"Particularly impressive results (80%+) in well-researched and professionally written critical analysis essays",
+				"Strong analytical skills developed on linguistic, cultural, and intercultural levels through study of literature, linguistics, history, art, and cinema",
+				"Recommended for Post-graduate study in Visual Arts, Film, or Literature",
+				"Spent a year abroad studying Spanish and Italian and teaching English in four different locations, gaining a wealth of interpersonal experience and becoming confident in my communication skills"
+			]
+		},
+		{
+			"location"				: "Sleaford Joint Sixth Form",
+			"description"			: "'A' Level, 'AS' Level",
+			"date"					: "Sep 2007 - Jul 2009",
+			"points"				: [
+				"'A' grade achieved in Maths, Spanish, French, and English Language at 'AS' and 'A' Level, 'A' grade in Critical Thinking at 'AS' Level (prior to introduction of A* grades)"
+			]
+		}
+	],
+	"skills" : [
+		{
+			"subheading"	: "Back end",
+			"points"		: [
+				"Broad experience of Node.js in multiple projects, coupled with MongoDB and Express to create robust server-side builds for websites and APIs",
+				"Worked extensively with WordPress to produce easily customisable themes; including handling legacy code and more complex multilingual builds",
+				"Experienced PHP and SQL user, especially with WordPress and Drupal builds",
+				"Created an eCommerce site using WooCommerce as my first self-taught project"
+			]
+		},
+		{
+			"subheading"	: "Front end",
+			"points"		: [
+				"Diligent user of HTML5 with a keen eye for compliant, semantic, and organised code",
+				"Experienced in converting designs to CSS3/SASS from PSD, and adapting existing styles to create functional, appealing, and responsive websites",
+				"Highly comfortable with front-end JavaScript & jQuery for interactivity, AJAX, & animation",
+				"Sharp eye for correct implementation of Bootstrap grid system",
+				"Use of Handlebars templating language to generate markup in client or on the server",
+				"Self-taught Angular 2 in order to present to team on its benefits and uses"
+			]
+		},
+		{
+			"subheading"	: "Other digital",
+			"points"		: [
+				"Regular, smart user of Git version control software with SourceTree",
+				"Practical user of Adobe PhotoShop and Illustrator",
+				"Trained in use of Google Analytics, Radian6 social analytics, and Google AdWords"
+			]
+		},
+		{
+			"subheading"	: "Soft skills",
+			"points"		: [
+				"Strong, comfortable communicator thanks to an academic background in modern languages and experience with internal and external clients at Edelman",
+				"Proficient project-juggler, comfortable time managing myself when needed for support on multiple projects in order to hit deadlines",
+				"Enthusiastic teacher who understands the importance of sharing knowledge to raise the level of a team as a whole",
+				"Highly capable both working independently on self-lead projects or as a team",
+				"Able to fulfil the role of tech leader on small projects, assisting with scoping, kick-off meetings, delegation, and planning",
+				"Utter geek - I love learning new skills and am naturally curious when it comes to technology, history, culture, gaming, linguistics, and cinema!"
+			]
+		},
+		{
+			"subheading"	: "Languages (natural)",
+			"points"		: [
+		       "English (native)", 
+		       "Spanish (fluent)",
+		       "Italian (fluent)",
+		       "French (good)"
+			]
+		}
+	]
 };
